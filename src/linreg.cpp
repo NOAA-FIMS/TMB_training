f50a777d8d574523aec9dd4029b1404c2bf4eb87
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-03-17 10:24:08 -0700

    initialize repository

---
+++ src/linreg.cpp
@@ -0,0 +1,25 @@
+// Simple linear regression
+#include <TMB.hpp>
+template <class Type>
+  
+Type objective_function<Type>::operator()()
+{
+  DATA_VECTOR(y);
+  DATA_MATRIX(X);
+  PARAMETER_VECTOR(beta);
+  PARAMETER(lnSigma);
+  
+  Type nll = 0;
+  Type sigma = exp(lnSigma);
+  int n = y.size();
+  vector<Type> mu = X * beta;
+  for(int i=0; i<n; i++){
+    nll -= dnorm(y(i), mu(i), sigma, true);
+  }
+  
+  Type sig2 = pow(sigma,2);
+  REPORT(sig2);
+  ADREPORT(sig2);
+  
+  return nll;
+}
