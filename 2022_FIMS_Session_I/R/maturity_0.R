839f769aab64dbdcea6d278b9abea78eadc394a4
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-08-01 23:27:57 -0700

    add maturity example

---
+++ 2022_FIMS_Session_I/R/maturity_0.R
@@ -0,0 +1,47 @@
+library(TMB)
+
+compile("2022_FIMS_Session_I/src/maturity_0.cpp")
+dyn.load(dynlib("2022_FIMS_Session_I/src/maturity_0"))
+
+a50 = 5
+slope = 2
+beta = c(-slope*a50,slope)
+ages = t(matrix(1:20, 20, 40))
+mat = 1/(1+exp(-(beta[1] + beta[2]*ages)))
+set.seed(123)
+N = matrix(sample(5:10, length(ages),replace=TRUE), 40, 20)
+Y = matrix(rbinom(length(N), N, mat), 40, 20)
+
+
+input = list(data=list(),par=list())
+input$par$beta = c(0,0)
+input$data$Y = c(Y)
+input$data$N = c(N)
+input$data$age_obs = c(ages)
+input$data$max_age = max(ages)
+
+mod = MakeADFun(input$data, input$par, DLL = "maturity_0")
+
+opt = nlminb(mod$par, mod$fn, mod$gr)
+mod$rep = mod$report()
+mod$sdrep = sdreport(mod)
+summary(mod$sdrep, "fixed")
+beta
+
+logit_mat = as.list(mod$sdrep, "Estimate", report = TRUE)$logit_mat_at_age
+logit_mat.se =as.list(mod$sdrep, "Std. Error", report = TRUE)$logit_mat_at_age
+logit_mat.ci =matrix(logit_mat, 20, 2) + cbind(-qnorm(0.975)*logit_mat.se, qnorm(0.975)*logit_mat.se)
+plot(ages[1,], 1/(1+exp(-logit_mat)), type = 'l', ylim = c(0,1))
+polygon(c(ages[1,],rev(ages[1,])), 1/(1 + exp(-c(logit_mat.ci[,1],rev(logit_mat.ci[,2])))), lty = 2)
+  
+## Map parameters
+input$map$beta = factor(c(1,NA)) 
+mod = MakeADFun(input$data, input$par, map = input$map)
+opt = nlminb(mod$par, mod$fn, mod$gr)
+opt$par
+
+#intercept = slope
+input$map$beta = factor(c(1,1)) 
+mod = MakeADFun(input$data, input$par, map = input$map)
+opt = nlminb(mod$par, mod$fn, mod$gr)
+opt$par
---
+++ 2022_FIMS_Session_I/src/maturity_0.cpp
@@ -0,0 +1,44 @@
+#include <TMB.hpp>
+#include <iostream>
+
+#define see(object) std::cout << #object ":\n" << object << "\n";
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+  DATA_VECTOR(Y); //number mature
+  DATA_VECTOR(N); //number of mature + not mature
+  DATA_VECTOR(age_obs); //age for each observation
+  DATA_INTEGER(max_age); //to generate a maturity ogive
+  PARAMETER_VECTOR(beta); //intercept, slope
+
+  int n_obs = Y.size();
+  vector<Type> nll(n_obs);
+  nll.setZero();
+     
+  vector<Type> logit_mat(n_obs), mat(n_obs);
+  for(int i = 0; i < n_obs; i++) {
+    logit_mat(i) = beta(0)  + beta(1)*age_obs(i); 
+    mat(i) = 1/(1 + exp(-logit_mat(i)));
+    nll(i) = -dbinom(Y(i), N(i), mat(i), 1);   //negative log-likelihood
+  }
+  
+  SIMULATE{
+    Y(i) = rbinom(N(i), mat(i));
+  }
+
+  //derived quantities
+  vector<Type> logit_mat_at_age(max_age);
+  for(int i = 0; i < max_age; i++) logit_mat_at_age(i) = beta(0)  + beta(1)*Type(i+1);
+  vector<Type> mat_at_age = 1/(1+ exp(-logit_mat_at_age));
+  Type a50 = -beta(0)/beta(1);
+  
+  REPORT(mat);
+  ADREPORT(a50);
+  REPORT(nll);
+  ADREPORT(logit_mat_at_age);
+  ADREPORT(mat_at_age);
+  
+  return sum(nll);
+}
+
