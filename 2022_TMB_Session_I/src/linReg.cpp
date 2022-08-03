5380d03a3951667887fc5573b64947d24205a2ee
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-08-03 11:26:49 -0700

    add simulate chunck

--- 2022_TMB_Session_I/src/linReg.cpp
+++ 2022_TMB_Session_I/src/linReg.cpp
@@ -16,6 +16,10 @@ Type objective_function<Type>::operator()()
   for(int i=0; i<n; i++){
     nll -= dnorm(y(i), mu(i), sigma, true);
   }
+  SIMULATE{
+    y = rnorm(mu, sigma);
+    REPORT(y);
+  }
   
   Type sig2 = pow(sigma,2);
   REPORT(sig2);
