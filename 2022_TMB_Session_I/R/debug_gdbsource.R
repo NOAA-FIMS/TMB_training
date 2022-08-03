e337c11d36d084882c48f63e23a9058c665293dd
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-08-03 11:49:11 -0700

    update debugging slides

--- 2022_TMB_Session_I/R/debug_gdbsource.R
+++ 2022_TMB_Session_I/R/debug_gdbsource.R
@@ -1,3 +1,12 @@
 library(TMB)
 
-compile('2022_TMB_Session_I/src/debug.cpp',"-O1 -g",DLLFLAGS="")
+compile('2022_TMB_Session_I/src/linReg.cpp',"-O1 -g",DLLFLAGS="")
+dyn.load(dynlib('2022_TMB_Session_I/src/linReg'))
+Data <- list(y = c(-2.1, 3.3, 4.2),
+             X = cbind(c(1,1,1),c(4,5,6))
+)
+
+Pars <- list(beta = 0, lnSigma = 0)
+obj <- MakeADFun(data = Data, 
+                 parameters = Pars,  
+                 DLL = "linReg")
--- 2022_TMB_Session_I/R/debugging.R
+++ 2022_TMB_Session_I/R/debugging.R
@@ -7,20 +7,11 @@ compile('2022_TMB_Session_I/src/debug1.cpp')
 #TMB 1.8.0: disable Eigen warnings
 compile('2022_TMB_Session_I/src/debug1.cpp', eigen.disable.warnings = TRUE)
 
-#Debug using gdbsource
-#hangs on Windows
-gdbsource("2022_TMB_Session_I/R/debug_gdbsource.R")
-#works on Windows with interactive mode
-gdbsource("2022_TMB_Session_I/R/debug_gdbsource.R", 
-                 interactive = TRUE)
-
-dyn.load(dynlib('2022_TMB_Session_I/src/linReg'))
-
 
 #------------------------------------------------------------------------
 # TMB-R linkage errors
 #------------------------------------------------------------------------
-
+library(TMB)
 #Simulate data 
 set.seed(123)
 sig <- 2
@@ -30,11 +21,28 @@ mu <- X%*%beta
 y = rnorm(50, mu, sig)
 
 
+# #Code will crash RStudio
+# Data <- list(y = c(-2.1, 3.3, 4.2),
+#              X = rbind(c(1,1),c(4,5))
+# )
+# 
+# Pars <- list(beta = 0)
+# obj <- MakeADFun(data = Data, 
+#                  parameters = Pars,  
+#                  DLL = "linReg")
+
+#Debug using gdbsource
+#hangs on Windows
+#gdbsource("2022_TMB_Session_I/R/debug_gdbsource.R")
+#works on Windows with interactive mode
+gdbsource("2022_TMB_Session_I/R/debug_gdbsource.R", 
+          interactive = TRUE)
+
 compile('2022_TMB_Session_I/src/linReg.cpp')
 Data <- list(y = c(-2.1, 3.3, 4.2),
-             X = data.frame(c(1,1),c(4,5))
+             X = data.frame(c(1,1,1),c(4,5,6))
 )
-Pars <- list(beta = 0)
+Pars <- list(beta = c(0,0))
 obj <- MakeADFun(data = Data, 
                  parameters = Pars,  
                  DLL = "linReg")
@@ -43,14 +51,6 @@ obj <- MakeADFun(data = Data,
 #---------------------------------------------------------------------
 # Run-time errors
 #--------------------------------------------------------------------
-Data <- list(y = c(-2.1, 3.3, 4.2),
-             X = cbind(c(1,1,1),c(4,5,2))
-)
-Pars <- list(beta = c(0,0), sigma = 0)
-obj <- MakeADFun(data = Data, 
-                 parameters = Pars, 
-                 DLL = "linReg")
-opt <- nlminb(obj$par, obj$fn, obj$gr)
 
 compile("2022_TMB_Session_I/src/debug2.cpp")
 dyn.load(dynlib("2022_TMB_Session_I/src/debug2"))
