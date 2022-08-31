31503dca8d07e818da881001fa20c48b632f5c75
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2022-08-31 11:43:57 -0700

    add randomwalk example

---
+++ 2022_TMB_Session_II/R/randomwalk.R
@@ -0,0 +1,77 @@
+#randomwalk example
+#dependencies: DHARMa, gap
+#install.packages("DHARMa")
+#install.packages("gap")
+library(TMB)
+N <- 100
+mu <- 2
+sd.vec <- c(1,1)
+
+# simulate random measurements and data ====================
+set.seed(123)
+u <- c(0,cumsum(rnorm(N-1,mean=mu,sd=sd.vec[2])))
+y <- u + rnorm(N,sd=sd.vec[1])
+
+
+# setup TMB model ==========================================
+compile('2022_TMB_Session_II/src/randomwalk.cpp')
+dyn.load(dynlib('2022_TMB_Session_II/src/randomwalk'))
+
+Dat <- list(y = y, sim_re = 0)
+Pars <- list( u = rep(1, N), mu = 0, 
+              ln_sig = 0, ln_tau = 0
+            )
+# Correctly specified model
+obj0 <- TMB::MakeADFun( data=Dat, parameters=Pars, 
+                       random="u", DLL="randomwalk" )
+opt0 <- nlminb(obj0$par, obj0$fn, obj0$gr)
+
+# Mis-specified model: fix mu=0
+obj1 <- TMB::MakeADFun( data=Dat, parameters=Pars,
+                        map = list(mu = factor(NA)),
+                        random="u", DLL="randomwalk" )
+opt1 <- nlminb(obj1$par, obj1$fn, obj1$gr)
+
+#OSA methods
+osa.fg.h0 <- oneStepPredict(obj0, observation.name = "y",
+                         method = "fullGaussian")
+osa.osg <- oneStepPredict(obj0, observation.name = "y",
+                          data.term.indicator = "keep",
+                          method = "oneStepGaussian")
+osa.cdf <- oneStepPredict(obj0, observation.name = "y",
+                          data.term.indicator = "keep",
+                          method = "cdf")
+osa.gen <- oneStepPredict(obj0, observation.name = "y",
+                          data.term.indicator = "keep",
+                          method = "oneStepGeneric")
+osa.fg.h1 <- oneStepPredict(obj1, observation.name = "y",
+                            method = "fullGaussian")
+qqnorm(osa.fg.h0$residual);abline(0,1)
+qqnorm(osa.fg.h1$residual);abline(0,1)
+
+#DHARMa methods
+#Only simulate from data model
+set.seed(123)
+obj0$env$data$sim_re <- 0
+sim.y <- replicate(100, {obj0$simulate()$y})
+ecdf.cond.h0 <- DHARMa::createDHARMa(sim.y, y)
+
+obj1$env$data$sim_re <- 0
+sim.y <- replicate(100, {obj1$simulate()$y})
+ecdf.cond.h1 <- DHARMa::createDHARMa(sim.y, y)
+
+#Simulate from data and RE model
+obj0$env$data$sim_re <- 1
+sim.y <- replicate(100, {obj0$simulate()$y})
+ecdf.uncond.h0 <- DHARMa::createDHARMa(sim.y, y, rotation = "estimated")
+
+obj1$env$data$sim_re <- 1
+sim.y <- replicate(100, {obj1$simulate()$y})
+ecdf.uncond.h1 <- DHARMa::createDHARMa(sim.y, y, rotation = "estimated")
+
+#Correctly specified model
+gap::qqunif(ecdf.cond.h0$scaledResiduals, logscale = FALSE);abline(0,1)
+gap::qqunif(ecdf.uncond.h0$scaledResiduals, logscale = FALSE);abline(0,1)
+#Mis-specified model
+gap::qqunif(ecdf.cond.h0$scaledResiduals, logscale = FALSE);abline(0,1)
+gap::qqunif(ecdf.uncond.h0$scaledResiduals, logscale = FALSE);abline(0,1)
---
+++ 2022_TMB_Session_II/src/randomwalk.cpp
@@ -0,0 +1,78 @@
+// Estimate and validate a random walk model with and without drift
+//
+// Compare Thygesen et al (submitted, 2016): Validation of state space models
+// fitted as mixed effects models
+//
+// Uffe Høgsbro Thygesen and Kasper Kristensen, 2016
+
+
+#include <TMB.hpp>
+
+template <class Type>
+Type objective_function<Type>::operator()()
+{
+  DATA_VECTOR(y);                  // Observations
+  DATA_INTEGER(sim_re);
+  DATA_VECTOR_INDICATOR(keep, y);  // For one-step predictions
+
+  //DATA_SCALAR(huge);
+  PARAMETER_VECTOR(u);
+  PARAMETER(mu);
+  PARAMETER(ln_sig);
+  PARAMETER(ln_tau);
+
+  Type sig=exp(ln_sig);	// observation sd
+  Type tau=exp(ln_tau);		// process sd
+  // Initial condition
+  Type nll = -dnorm(u(0), Type(0), Type(1000), true);
+
+  // Increments
+  vector<Type> ypred(u.size());
+  ypred.setZero();
+  ypred(0) = u(0);
+  SIMULATE{
+    u(0) = rnorm(u(0), tau); 
+  }
+  for (int i = 1; i < u.size(); ++i){
+    ypred(i) = u(i - 1) + mu;
+    nll -= dnorm(u(i), ypred(i), tau, true);
+    if(sim_re == 1){
+      SIMULATE{
+        u(i) = rnorm(ypred(i), tau);
+      }
+    }
+  }
+
+  // Observations
+  Type cdf = 0;
+  for (int i = 0; i < y.size(); ++i){
+    nll -= keep(i) * dnorm(y(i), u(i), sig, true);
+    cdf = squeeze( pnorm(y(i), u(i), sig) );
+    nll -= keep.cdf_lower(i) * log( cdf );
+    nll -= keep.cdf_upper(i) * log( 1.0 - cdf );
+    SIMULATE {
+      y(i) = rnorm(u(i), sig); // conditional
+    }
+  }
+  
+  SIMULATE{
+    if(sim_re==1){
+      REPORT(u);
+    }
+    REPORT(y);
+  }
+  
+  vector<Type> fpr(y.size());
+  for(int i=0; i<y.size(); i++) fpr(i) = mu;
+  vector<Type> exp_val = u;
+  
+  REPORT(u);
+  REPORT(exp_val);
+  REPORT(fpr);
+  REPORT(ypred);
+  REPORT(tau);
+  REPORT(sig);
+  REPORT(nll);
+  
+  return nll;
+}
